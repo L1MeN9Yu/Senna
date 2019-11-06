5f40156fd7d10a12a9798026263a5efdfe658a86
L1MeN9Yu <baal998.mophisto@gmail.com>
2019-11-06 15:03:48 +0800

    LogFlag add init?(unsignedIntValue: CUnsignedInt)

--- Source/Frontend/LogFlag.swift
+++ Source/Frontend/LogFlag.swift
@@ -13,6 +13,27 @@ public enum LogFlag {
     case error
     case critical
     case off
+
+    public init?(unsignedIntValue: CUnsignedInt) {
+        switch unsignedIntValue {
+        case 0:
+            self = .trace
+        case 1:
+            self = .debug
+        case 2:
+            self = .info
+        case 3:
+            self = .warning
+        case 4:
+            self = .error
+        case 5:
+            self = .critical
+        case 6:
+            self = .off
+        default:
+            return nil
+        }
+    }
 }
 
 public extension LogFlag {
