c8831a79b05bb59066f354acb27b23d719eec64d
L1MeN9Yu <baal998.mophisto@gmail.com>
2020-03-11 11:38:45 +0800

    fix shell
    Signed-off-by: L1MeN9Yu <baal998.mophisto@gmail.com>

--- ios_mac.sh
+++ ios_mac.sh
@@ -1,5 +1,10 @@
-rm -r build
+rm -r build Senna.xcframework
 xcodebuild archive -scheme "Senna.iOS" -sdk iphoneos OBJROOT=build/iOS
 xcodebuild archive -scheme "Senna.iOS" -sdk iphonesimulator OBJROOT=build/simulator
-xcodebuild -create-xcframework -framework build/iOS/UninstalledProducts/iphoneos/Senna.framework -framework build/simulator/UninstalledProducts/iphonesimulator/Senna.framework -output build/Senna.xcframework
+xcodebuild archive -scheme "Senna.iOS" -destination 'platform=macOS,arch=x86_64,variant=Mac Catalyst'  OBJROOT=build/maccatalyst
+xcodebuild -create-xcframework \
+-framework build/iOS/UninstalledProducts/iphoneos/Senna.framework \
+-framework build/simulator/UninstalledProducts/iphonesimulator/Senna.framework \
+-framework build/maccatalyst/UninstalledProducts/macosx/Senna.framework \
+-output build/Senna.xcframework
 mv build/Senna.xcframework Senna.xcframework
