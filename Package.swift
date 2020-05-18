aef0e4db953159c227148614dc513b57aa4a4db7
L1MeN9Yu <baal998.mophisto@gmail.com>
2020-05-18 11:00:00 +0800

    use submodule

--- Package.swift
+++ Package.swift
@@ -13,9 +13,7 @@ let package = Package(
         targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-            .target(
-                    name: "Senna.spdlog"
-            ),
+            .target(name: "Senna.spdlog"),
             .target(name: "Senna", dependencies: ["Senna.spdlog"]),
             .testTarget(name: "SennaTests", dependencies: ["Senna"]),
         ],
