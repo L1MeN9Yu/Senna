11c10ea538d9a9d4236e1a54566f8e0c57d90501
L1MeN9Yu <baal998.mophisto@gmail.com>
2020-05-27 11:13:38 +0800

    update spdlog version to 1.6.1

--- Tests/SennaTests/SennaTests.swift
+++ Tests/SennaTests/SennaTests.swift
@@ -6,6 +6,21 @@ final class SennaTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        class Logger: Senna.Logger {
+            let name: String = "Test"
+
+            init() { self.register() }
+
+            deinit { self.drop() }
+        }
+
+        let logger = Logger()
+        logger.trace("trace")
+        logger.debug("debug")
+        logger.info("info")
+        logger.warn("error")
+        logger.error("error")
+        logger.crit("crit")
     }
 
     static var allTests = [
