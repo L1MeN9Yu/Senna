804875a32695197543c964ec93aaac85af467715
L1MeN9Yu <baal998.mophisto@gmail.com>
2020-07-07 17:28:15 +0800

    update package

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
         name: "Senna",
         platforms: [
-            SupportedPlatform.iOS(.v9),
-            SupportedPlatform.macOS(.v10_13),
+            SupportedPlatform.iOS(.v10),
+            SupportedPlatform.macOS(.v10_14),
         ],
         products: [
             // Products define the executables and libraries produced by a package, and make them visible to other packages.
