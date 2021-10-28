7eefe61736244b12ef47169077f5c17d5f570048
L1MeN9Yu <baal998.mophisto@gmail.com>
2021-10-28 17:29:25 +0800

    [Tests] : System add MetaData

--- Tests/SystemTests.swift
+++ Tests/SystemTests.swift
@@ -18,4 +18,19 @@ final class SystemTests: XCTestCase {
         logger.error("\(UInt8.random(in: .min ..< .max))")
         logger.critical("\(UInt8.random(in: .min ..< .max))")
     }
+
+    func testMetaData() {
+        var logger = Logger(label: "syslog") {
+            Handler(name: $0, sink: System(), formatter: Formatter.os, logLevel: .trace)
+        }
+        logger[metadataKey: "UserName"] = .string("L1MeN9Yu")
+        logger[metadataKey: "UserID"] = .stringConvertible(9527)
+        logger.trace("\(UInt8.random(in: .min ..< .max))", metadata: ["Key": .dictionary(["String": .stringConvertible(0)])])
+        logger.debug("\(UInt8.random(in: .min ..< .max))")
+        logger.info("\(UInt8.random(in: .min ..< .max))")
+        logger.notice("\(UInt8.random(in: .min ..< .max))")
+        logger.warning("\(UInt8.random(in: .min ..< .max))")
+        logger.error("\(UInt8.random(in: .min ..< .max))")
+        logger.critical("\(UInt8.random(in: .min ..< .max))", metadata: ["Key": .dictionary(["String": .stringConvertible(6)])])
+    }
 }
