745b5b0042997b86eaa9a73cca974b276f61fead
L1MeN9Yu <baal998.mophisto@gmail.com>
2022-03-04 11:15:08 +0800

    [Formation] : move protocol function to implement

--- Sources/Implements/Formation.swift
+++ Sources/Implements/Formation.swift
@@ -49,6 +49,41 @@ public struct Formation: Formable {
     }
 }
 
+private extension Formation {
+    // swiftlint:disable function_parameter_count
+    func format(name: String, component: Component, date: Date, level: Logger.Level, message: Logger.Message, prettyMetadata: String?, source: String, file: String, function: String, line: UInt) -> String {
+        switch component {
+        case .name:
+            return "<\(name)>"
+        case let .timestamp(dateFormatter):
+            return dateFormatter.string(from: date)
+        case .level:
+            return "\(level)"
+        case .message:
+            return "\(message)"
+        case .metadata:
+            return "\(prettyMetadata.map { "\($0)" } ?? "")"
+        case .source:
+            return "\(source)"
+        case .file:
+            return "\(URL(fileURLWithPath: file).lastPathComponent)"
+        case .function:
+            return "\(function)"
+        case .line:
+            return "\(line)"
+        case let .text(value):
+            return value
+        case let .group(components):
+            return components.map {
+                format(name: name, component: $0, date: date, level: level, message: message, prettyMetadata: prettyMetadata, source: source, file: file, function: function, line: line)
+            }
+            .joined()
+        }
+    }
+
+    // swiftlint:enable function_parameter_count
+}
+
 public extension Formation {
     static var standard: Self {
         Formation(
